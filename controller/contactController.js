6dff6b13cc56b67db8a9edc61fd40ba26ae3b71c
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-05-14 00:10:31 +0530

    modify controller with handling error

--- controller/contactController.js
+++ controller/contactController.js
@@ -3,16 +3,22 @@ const getContact = (req, res) => {
 };
 
 const createContact = (req, res) => {
-    res.status(201).json({message:" create a new contact"})
+    console.log("req.body", req.body);
+    const { name, phone, email} = req.body;
+    if(!name || !phone ||!email){
+        res.status(400);
+        throw new Error("All fields are mandatory man !")
+    }
+    res.status(201).json({ message: "create a new contact" });
 };
 
 const updateContact = (req, res) => {
-    res.status(201).json({message:"update successfully !"})
+    res.status(201).json({ message: "update successfully !" })
 };
 
 const deleteContact = (req, res) => {
-    console.log("here req body is ", req.body);
-    res.status(200).json({message:`delete id is ${req.params.id}`})
+    console.log("here req body is ");
+    res.status(200).json({ message: `delete id is ${req.params.id}` })
 };
 
-module.exports = {getContact, createContact, updateContact, deleteContact}
+module.exports = { getContact, createContact, updateContact, deleteContact };
